e6bd2823ab5c0990d9332d582d862913a24a703e
Robert Jewell <rjewellaudio@gmail.com>
2021-02-02 22:06:12 +0100

    fix "show more" button text change issue

--- js/deseryCups-expand.js
+++ js/deseryCups-expand.js
@@ -1,12 +1,12 @@
-const btn = document.querySelector("#desery__button");
+let btn = document.querySelector("#desery__button");
 const fadeCover = document.querySelector(".desery--cups--fade");
 const extraOptions = document.querySelector(".desery--cups--hidden");
 
 btn.addEventListener("click", () => {
-  if ((btn.innerHTML = "see more")) {
-    btn.innerHTML = "hide";
+  if (btn.textContent === "see more") {
+    btn.textContent = "hide";
   } else {
-    btn.innerHTML = "see more";
+    btn.textContent = "see more";
   }
   fadeCover.classList.toggle("desery--cups--fade");
   extraOptions.classList.toggle("desery--cups--hidden");
