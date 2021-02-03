8c8ba84db70b4067b760b75525cfac83e6a8fdec
Robert Jewell <rjewellaudio@gmail.com>
2021-02-03 15:11:17 +0100

    remove tinySwiper js file

--- js/tinySwiper.js
+++
@@ -1,8 +0,0 @@
-import Swiper, {
-    SwiperPluginLazyload,
-    SwiperPluginPagination
-} from 'tiny-swiper'
-
-Swiper.use([ SwiperPluginLazyload, SwiperPluginPagination ])
-
-const swiper = new Swiper(swiperContainer: HTMLElement | string, parameters?: TinySwiperParameters)
