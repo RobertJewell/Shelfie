f42da63966d9db8671fea7f49fae40ee130834f0
Robert Jewell <rjewellaudio@gmail.com>
2021-02-15 13:53:16 +0100

    Update show more button with Polish

--- js/deseryCups-expand.js
+++ js/deseryCups-expand.js
@@ -3,10 +3,10 @@ const fadeCover = document.querySelector(".desery--cups--fade");
 const extraOptions = document.querySelector(".desery--cups--hidden");
 
 btn.addEventListener("click", () => {
-  if (btn.textContent === "see more") {
-    btn.textContent = "hide";
+  if (btn.textContent === "Więcej opcji") {
+    btn.textContent = "Mniej opcji";
   } else {
-    btn.textContent = "see more";
+    btn.textContent = "Więcej opcji";
   }
   fadeCover.classList.toggle("desery--cups--fade");
   extraOptions.classList.toggle("desery--cups--hidden");
